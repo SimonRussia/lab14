7351996fad368329859727ef7515e0d648d819c8
SimonRussia <git.streisand@gmail.com>
2017-12-06 23:37:44 +0300

    Update process.cpp

--- sources/process.cpp
+++ sources/process.cpp
@@ -1,36 +1,129 @@
-#include <boost/process.hpp>
-#include <boost/system/error_code.hpp>
-#include <boost/asio.hpp>
-// #include <boost/process/child.hpp>
-// #include <boost/process/cmd.hpp>
-// #include <boost/process/group.hpp>
-// #include <boost/process/pipe.hpp>
-// #include <boost/process/posix.hpp>
-// #include <boost/process/system.hpp>
 #include <iostream>
-#include <thread>
+#include <chrono>
+#include <boost/program_options.hpp>
+#include <boost/process.hpp>
 
 namespace bp = boost::process;
+namespace po = boost::program_options;
 
-std::vector<std::string> read_outline(std::string & file) {
-	    bp::ipstream is; //reading pipe-stream
-	    bp::child c(bp::search_path("nm"), file, bp::std_out > is);
-
-	    std::vector<std::string> data;
-	    std::string line;
-
-	    while (c.running() && std::getline(is, line) && !line.empty())
-	        data.push_back(line);
-
-	    c.wait();
-
-	    return data;
-	}
-
-int main()
+bp::child Make(int time = 0, std::string build="Debug")
 {
-	int result = boost::system("g++ main.cpp");
-	std::cout << result << std::endl;
+  // std::string cmd("cd examples && cmake -H. -B_build -DCMAKE_INSTALL_PREFIX=_install -DCMAKE_BUILD_TYPE=");
+  // cmd += build;
 
-	
+  std::string cmd("cd examples && cmake -H. -B_build");
+  // std::cout << "command = " << cmd << '\n';
+  bp::child c(cmd, bp::std_out > stdout);
+  if (time) {
+    if (!c.wait_for(std::chrono::seconds(time)));
+      c.terminate();
+  } else {
+    c.wait();
+  }
+  return c;
+}
+
+bp::child Build(int time = 0)
+{
+  std::string cmd("cd examples && cmake --build _build");
+
+  // std::cout << "command = " << cmd << '\n';
+  bp::child c(cmd, bp::std_out > stdout);
+  if (time){
+    if (!c.wait_for(std::chrono::seconds(time)));
+      c.terminate();
+  } else {
+    c.wait();
+  }
+  return c;
+}
+
+bp::child Target(std::string inst)
+{
+  std::string cmd("cd examples && cmake --build _build");
+  cmd += " --target ";
+  cmd += inst;
+
+  // std::cout << "command = " << cmd << '\n';
+  bp::child c(cmd, bp::std_out > stdout);
+  c.wait();
+  return c;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    po::options_description desc("Allowed options");
+    desc.add_options()
+      ("help, h", "выводим вспомогательное сообщение")
+      ("config", po::value<std::string>(), "указываем конфигурацию сборки (по умолчанию Debug)")
+      ("install", "добавляем этап установки\n(в директорию _install)")
+      ("pack", "добавляем этап упакови\n(в архив формата tar.gz)")
+      ("timeout", po::value<int>(), "указываем время ожидания\n(в секундах)")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    int res;
+    if(vm.count("help"))
+    {
+      std::cout << desc << std::endl;
+    }
+
+    //  Обрабатываем конфигурацию сборки <Release|Debug>
+    else if (vm.count("config"))
+    {
+      std::string par(vm["config"].as<std::string>());
+
+      if (Make(0, par).exit_code())
+      	std::cerr << "make\truntime error" << std::endl;
+
+      std::cout << "code == " << Build().exit_code() << std::endl;
+    }
+    
+    //  Обрабатываем этап установки в директорию _install
+    else if (vm.count("install"))
+    {
+      if (Make().exit_code())
+      	std::cerr << "make\truntime error" << std::endl;
+
+      if (Build().exit_code())
+      	std::cerr << "build\truntime error" << std::endl;
+
+      std::cout << "code == " << Target("install").exit_code() << std::endl;
+    }
+
+    //  Обрабатываем этап упаковки в архив формата tar.gz
+    else if (vm.count("pack"))
+    {
+      if (Make().exit_code())
+      	std::cerr << "make\truntime error" << std::endl;
+
+      if (Build().exit_code())
+      	std::cerr << "build\truntime error" << std::endl;
+
+      std::cout << "code == " << Target("package").exit_code() << std::endl;
+    }
+
+    //  Обрабатываем время ожидания
+    else if (vm.count("timeout"))
+    {
+      int tim = vm["timeout"].as<int>();
+
+      if (Make(tim).exit_code())
+      	std::cerr << "make\truntime error" << std::endl;
+
+      std::cout << "code == " << Build(tim).exit_code() << std::endl;
+    }
+
+    //  default buld
+    else {
+      if (Make().exit_code())
+        std::cerr << "make\truntime error" << std::endl;
+
+      std::cout << "code == " << Build().exit_code() << std::endl;
+    }
+    return 0;
 }
